3df00d6d9c20279294397e5395a6e7aa3bd9e048
Nathan Lapinski <n4t311@gmail.com>
2019-01-28 20:07:33 +0900

    Vocab form component starter

---
+++ src/vocab/vocab-add-form/VocabAddForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './VocabAddForm.css'
+
+const VocabAddForm = () => (
+  <div>
+    <input type='text'></input>
+    <input type='text'></input>
+    <input type='text'></input>
+    <button>Add</button>
+  </div>
+)
+
+export default VocabAddForm
