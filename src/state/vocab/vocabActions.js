61a50f452e3b138142cb79c92e247c456b0cbcfa
Nathan Lapinski <n4t311@gmail.com>
2019-01-29 18:44:54 +0900

    creates an action for adding new vocab, and updates the vocabReducer to process this action

--- src/state/vocab/vocabActions.js
+++ src/state/vocab/vocabActions.js
@@ -1,12 +1,21 @@
 export const VOCAB_WORDS_REQUEST = 'VOCAB_WORDS_REQUEST'
 export const VOCAB_WORDS_REQUEST_SUCCESS = 'VOCAB_WORDS_REQUEST_SUCCESS'
 export const VOCAB_WORDS_REQUEST_ERROR = 'VOCAB_WORDS_REQUEST_ERROR'
+export const VOCAB_WORDS_CREATE = 'VOCAB_WORDS_CREATE'
 
 export const vocabWordsRequest = () => ({ type: VOCAB_WORDS_REQUEST })
 export const vocabWordsRequestSuccess = ({words}) => ({ 
-    type: VOCAB_WORDS_REQUEST_SUCCESS,
-    payload: {
-        data: words
-    }
+  type: VOCAB_WORDS_REQUEST_SUCCESS,
+  payload: {
+    data: words
+  }
 })
 export const vocabWordsRequestError = () => ({ type: VOCAB_WORDS_REQUEST_ERROR })
+export const vocabWordsCreate = ({word, reading, answer}) => ({
+  type: VOCAB_WORDS_CREATE,
+  payload: {
+    word,
+    reading,
+    answer
+  }
+})
--- src/state/vocab/vocabReducer.js
+++ src/state/vocab/vocabReducer.js
@@ -1,10 +1,20 @@
-import { VOCAB_WORDS_REQUEST_SUCCESS } from './vocabActions'
+import { VOCAB_WORDS_REQUEST_SUCCESS, VOCAB_WORDS_CREATE } from './vocabActions'
 export const vocabReducer = (state=[], action) => {
   switch (action.type) {
     case VOCAB_WORDS_REQUEST_SUCCESS:
       return [
         ...state,
         ...action.payload.data
+      ]
+    case VOCAB_WORDS_CREATE:
+      const {word, reading, answer} = action.payload
+      return [
+        ...state,
+        {
+          word,
+          reading,
+          answer
+        }
       ]
     default:
       return state;
