c0cfe852440220525a1c2994729e7b64c7a14798
Nathan Lapinski <n4t311@gmail.com>
2019-01-29 18:11:24 +0900

    adds functionality for writing all state changes to localStorage, and for hydrating the application with localStorage data

--- src/state/store.js
+++ src/state/store.js
@@ -1,14 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+
+import { loadState, saveState } from '../localStorage/localStorage'
+
 import { vocabReducer } from './vocab/vocabReducer';
 import vocabRequestSaga from './vocab/vocabMiddleware';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const persistedState = loadState()
 const store = createStore(
   vocabReducer,
+  persistedState,
   applyMiddleware(sagaMiddleware)
 )
+
+// write the state to local storage each time the store updates
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 sagaMiddleware.run(vocabRequestSaga)
 
