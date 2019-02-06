2b22548b4d6c9d3798c5cd788d10a0777e1b87c0
Nathan Lapinski <n4t311@gmail.com>
2019-02-06 17:23:08 +0900

    adding styles for vocab box remove button

--- src/vocab/vocab-box/VocabBox.js
+++ src/vocab/vocab-box/VocabBox.js
@@ -4,6 +4,7 @@ import './VocabBox.css'
 
 const VocabBox = ({word, answer, reading}) => (
   <div className="vocab-box">
+    <div className="vocab-box-remove">X</div>
     <h3 className="vocab-box-word">{word}</h3>
     <h5 className="vocab-box-reading">{reading}</h5>
     <h4 className="vocab-box-answer">{answer}</h4>
